9b3c5a5508988a225adc833bd23841b42640ba71
kolomoetsv <52840884+kolomoetsv@users.noreply.github.com>
2021-05-05 20:44:09 +0300

    optimised code

--- courses/catalog/views.py
+++ courses/catalog/views.py
@@ -1,9 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import CourseCreateForm
 from django.views.generic import ListView, DetailView, DeleteView, UpdateView
-from django import forms
 
+from .forms import CourseCreateForm
 from .models import Course
 
 
@@ -23,6 +23,7 @@ def course_create(request):
     return render(request, 'form-add-course.html', {'form': form})
 
 
+
 class CourseListView(ListView):
     queryset = Course.objects.all()
     template_name = 'index.html'
@@ -38,7 +39,7 @@ class CourseListView(ListView):
             queryset = queryset.filter(title__icontains=query)
         elif start_date:
             queryset = queryset.filter(start_date__gte=start_date)
-        elif start_date:
+        elif end_date:
             queryset = queryset.filter(end_date__lte=end_date)
 
         return queryset
@@ -52,19 +53,6 @@ class CourseDetailView(DetailView):
 class CourseDetailEdit(DetailView):
     queryset = Course.objects.all()
     template_name = 'course-detail.html'
-
-
-# add new course to the database
-def add_course(request):
-    if request.method == "POST" and request.POST.get('title'):
-        Course.objects.create(
-            title=request.POST.get('title'),
-            start_date=request.POST.get('start_date'),
-            end_date=request.POST.get('end_date'),
-            number_of_lectures=request.POST.get('number_of_lectures'))
-
-        messages.add_message(request, messages.INFO, 'Курс успешно добавлен')
-    return render(request, 'form-add-course.html')
 
 
 # delete a course
